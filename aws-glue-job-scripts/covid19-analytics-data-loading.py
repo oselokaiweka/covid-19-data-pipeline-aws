14633ae3efee3c3a8f2ef2b1939607b69a77f08d
Oseloka I <115153490+oselokaiweka@users.noreply.github.com>
2024-08-14 11:41:14 +0100

    Synchronizing job covid19-analytics-data-loading from AWS Glue at 2024/08/14 10:41:13

---
+++ aws-glue-job-scripts/covid19-analytics-data-loading.py
@@ -0,0 +1,188 @@
+import sys
+import time
+import boto3
+import logging
+import configparser
+import pandas as pd
+import awswrangler as wr
+import redshift_connector
+from pyspark.sql import SparkSession
+from botocore.exceptions import ClientError
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+  
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+
+sys.argv += ["--JOB_NAME", "covid19-analytics-data-warehouse-loading"]
+sys.argv += ["--CONFIG_BUCKET_NAME", "jk-config-s3"]
+sys.argv += ["--CONFIG_FILE_KEY", "covid19-analytics-config/covid19-analytics.config"]
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s %(levelname) %(message)s'
+)
+logger = logging.getLogger(__name__)
+# Access Glue job parameters
+args = getResolvedOptions(
+    sys.argv, [
+        'JOB_NAME', 
+        'CONFIG_BUCKET_NAME', 
+        'CONFIG_FILE_KEY',
+    ]
+)
+# Initialize job, setup parameters,logging and other environment settings
+job.init(args['JOB_NAME'],args)
+
+# Specify the s3 bucket and file pathS from Glue Job parameters
+config_bucket_name = args['CONFIG_BUCKET_NAME']  
+config_file_key = args['CONFIG_FILE_KEY'] 
+
+# Initialize s3 client to access config file
+config_s3_client = boto3.client('s3') # Grant glue access with IAM role
+
+# Read the config file from s3
+config_obj = config_s3_client.get_object(Bucket=config_bucket_name, Key=config_file_key)
+config_data = config_obj['Body'].read().decode('utf-8')
+
+# Parse the config file
+config = configparser.ConfigParser()
+config.read_string(config_data)
+
+# Access parameters stored in config file
+aws_key = config.get('AWS', 'KEY')
+aws_secret = config.get('AWS', 'SECRET')
+job_region = config.get('S3', 'JOB_REGION')
+job_bucket_name = config.get('S3', 'JOB_BUCKET_NAME')
+job_output_prefix = config.get('S3', 'JOB_OUTPUT_PREFIX')
+job_output_path = config.get('S3', 'JOB_OUTPUT_PATH')
+crawler_name = config.get('GLUE', 'CRAWLER2')
+crawler_roleArn = config.get('GLUE', 'CRAWLER_ROLE')
+schema_name = config.get('GLUE', 'SCHEMA_NAME2')
+db_password = config.get('DWH', 'DWH_DB_PASSWORD')
+cluster_identifier = config.get('DWH', 'DWH_CLUSTER_IDENTIFIER')
+
+# Set up the boto3 session
+session = boto3.Session(
+    region_name=job_region,
+    aws_access_key_id=aws_key, 
+    aws_secret_access_key=aws_secret
+)
+# Initialize Glue client to access and retrieve schema table list
+glue_client = session.client('glue')
+
+# Check if crawler exists, creates crawler if not found
+try:
+    crawler_response = glue_client.get_crawler(Name=crawler_name)
+    logger.info("Crawler '%s' already exists.", crawler_name)
+    print('Crawler exists')
+except ClientError as e:
+    logger.warning("'%s' glue crawler object not found. Creating crawler...", crawler_name)
+
+    try:
+        crawler_response = glue_client.create_crawler(
+            Name=crawler_name,
+            Role=crawler_roleArn,
+            DatabaseName=schema_name,
+            TablePrefix='',  # No prefix needed
+            RecrawlPolicy={'RecrawlBehavior': 'CRAWL_EVERYTHING'},
+            SchemaChangePolicy={
+                'UpdateBehavior': 'UPDATE_IN_DATABASE', 
+                'DeleteBehavior': 'DEPRECATE_IN_DATABASE'
+            },
+            Targets={
+                'S3Targets': [
+                    {
+                        'Path': fr"s3://{job_bucket_name}/{job_output_prefix}/",
+                        'Exclusions': ['**.json'] # Double ** matches all .json files in current folder and all subfolders
+                    }
+                ]
+            }
+        )
+        logger.info("Glue crawler '%s' has been created successfully.", crawler_name)
+        print('Crawler created')
+
+    except ClientError as create_error:
+        logger.error("Failed to create crawler '%s'. Error: %s", crawler_name, str(create_error), exc_info=True)
+
+# Start crawler
+glue_client.start_crawler(Name=crawler_name)
+logger.info("\n'%s' crawler has been started.", crawler_name)
+print('crawler started')
+
+# Wait for crawler to finish cataloging target data
+while True:
+    crawler_status = glue_client.get_crawler(Name=crawler_name)['Crawler']['State']
+    if crawler_status in ['READY', 'SUCCEEDED']:
+        break
+    time.sleep(15)  # Wait for 15 seconds before checking again
+
+# Fetch list of tables created by crawler
+tables = glue_client.get_tables(DatabaseName=schema_name)['TableList']
+table_count = len(tables)
+print(table_count)
+logger.info("\nTotal of %s created by crawler", table_count)
+# Loop through tables
+
+dfs = {} # Store each {table_name : dataframe}
+s3_paths = {} # Store each {table_name : s3_path}
+
+for table in tables:
+    
+    # Use GlueContext to create a dynamic frame 
+    dynamic_frame = glueContext.create_dynamic_frame.from_catalog(
+        database=schema_name, 
+        table_name=table['Name'], 
+        transformation_ctx=f"dynamic_frame_{table['Name']}"
+    )
+    # Map data loccation to dynamic frame
+    table_metadata = glue_client.get_table(DatabaseName=schema_name, Name=table['Name'])
+    s3_path = table_metadata['Table']['StorageDescriptor']['Location']
+    s3_paths[fr"{table['Name']}"] = s3_path
+    
+    # Convert to PySpark Dataframe to leverage awswrangler
+    df = dynamic_frame.toDF()
+    dfs[fr"{table['Name']}"] = df
+    
+for table_name, df in dfs.items():
+    print(table_name)
+    df.show(3)
+    
+for table_name, s3_path in s3_paths.items():
+    print(f"{table_name}: {s3_path}")
+redshift_client = session.client('redshift')
+
+cp = redshift_client.describe_clusters(ClusterIdentifier=cluster_identifier)['Clusters'][0]
+dbname = cp['DBName']
+user = cp['MasterUsername']
+host = cp['Endpoint']['Address']
+port = int(cp['Endpoint']['Port'])
+
+# Connect to Redshift
+con = redshift_connector.connect(
+    database=dbname,
+    user=user,
+    password=db_password,
+    host=host,
+    port=port
+)
+
+for table_name, df in dfs.items():
+    wr.redshift.copy_from_files(
+        path=s3_paths[table_name],
+        con=con,
+        table=table_name[:-4],
+        schema=schema_name,  
+        iam_role=crawler_roleArn,
+        delimiter=',',   
+        ignore_headers=1, 
+        boto3_session=session
+        # Add more COPY command parameters if needed
+    )
+job.commit()
